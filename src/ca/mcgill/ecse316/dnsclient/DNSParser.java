ecf4ab131e321120eebf47b6fcead030e901dadf
Nicolas <jose.barreyrolopez@mail.mcgill.ca>
2020-02-11 02:33:24 -0500

    adding packet header structure

--- src/ca/mcgill/ecse316/dnsclient/DNSParser.java
+++ src/ca/mcgill/ecse316/dnsclient/DNSParser.java
@@ -2,7 +2,7 @@ package ca.mcgill.ecse316.dnsclient;
 
 import java.util.Arrays;
 
-public class DNSParser {
+public class DnsParser {
 	
 	public static void parse(String[] args) throws Exception {
 
@@ -12,14 +12,14 @@ public class DNSParser {
 			throw new InputSyntaxException("Insufficient number of arguments");
 
 		// Second to last argument is DNS server IPv4 address
-		DNSClient.dnsServerAddr = args[args.length - 2];
+		DnsClient.dnsServerAddr = args[args.length - 2];
 		
 		//First character should be '@'
-		if (DNSClient.dnsServerAddr.charAt(0) != '@')
+		if (DnsClient.dnsServerAddr.charAt(0) != '@')
 			throw new InputSyntaxException("Incorrect DNS server address");
-		DNSClient.dnsServerAddr = DNSClient.dnsServerAddr.substring(1);		//Get rid of '@'
+		DnsClient.dnsServerAddr = DnsClient.dnsServerAddr.substring(1);		//Get rid of '@'
 			
-		tempDnsAddr = DNSClient.dnsServerAddr.split("\\.");
+		tempDnsAddr = DnsClient.dnsServerAddr.split("\\.");
 		
 		// DNS server address must be of the form a.b.c.d
 		if (tempDnsAddr.length != 4)
@@ -35,7 +35,7 @@ public class DNSParser {
 		}
 
 		// Last argument is domain name
-		DNSClient.domName = args[args.length - 1];
+		DnsClient.domName = args[args.length - 1];
 		
 		// Parse optional flags
 		if (args.length > 2) 
@@ -55,36 +55,36 @@ public class DNSParser {
 			
 			case "-t" :
 				if (!(i + 1 < flags.length)) throw new InputSyntaxException("Missing timeout value");
-				try { DNSClient.timeout = Integer.valueOf(flags[i+1]); } 
+				try { DnsClient.timeout = Integer.valueOf(flags[i+1]); } 
 				catch (Exception e) { throw new InputSyntaxException("Invalid timeout value"); }
 				i += 2;
 				break;
 				
 			case "-r" :
 				if (!(i + 1 < flags.length)) throw new InputSyntaxException("Missing max retries value");
-				try { DNSClient.maxRetries = Integer.valueOf(flags[i+1]); } 
+				try { DnsClient.maxRetries = Integer.valueOf(flags[i+1]); } 
 				catch (Exception e) { throw new InputSyntaxException("Invalid max retries value"); }
 				i += 2;
 				break;
 				
 			case "-p" :
 				if (!(i + 1 < flags.length)) throw new InputSyntaxException("Missing port value");
-				try { DNSClient.port = Integer.valueOf(flags[i+1]); } 
+				try { DnsClient.port = Integer.valueOf(flags[i+1]); } 
 				catch (Exception e) { throw new InputSyntaxException("Invalid port value"); }
 				i += 2;
 				break;
 			
 			case "-mx":
-				if (DNSClient.queryType == QueryType.NS)
+				if (DnsClient.queryType == QueryType.NS)
 					 throw new InputSyntaxException("Only one of the mx/ns flags might be used");	
-				DNSClient.queryType = QueryType.MX;
+				DnsClient.queryType = QueryType.MX;
 				i++;
 				break;
 			
 			case "-ns":
-				if (DNSClient.queryType == QueryType.MX)
+				if (DnsClient.queryType == QueryType.MX)
 					 throw new InputSyntaxException("Only one of the mx/ns flags might be used");	
-				DNSClient.queryType = QueryType.NS;
+				DnsClient.queryType = QueryType.NS;
 				i++;
 				break;
 				
