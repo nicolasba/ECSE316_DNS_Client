b359a07a5aab7cbf6dfd8f1e22cc6c53381374fb
Nicolas <jose.barreyrolopez@mail.mcgill.ca>
2020-02-14 20:50:52 -0500

    final version

--- src/ca/mcgill/ecse316/dnsclient/DnsClient.java
+++ src/ca/mcgill/ecse316/dnsclient/DnsClient.java
@@ -64,7 +64,7 @@ public class DnsClient {
 
 			} catch (Exception e) {
 				System.out.println("ERROR\t" + e.getMessage());
-				System.exit(1);
+//				System.exit(1);
 			}
 
 		}
@@ -89,7 +89,7 @@ public class DnsClient {
 			System.out.println("ERROR\t Domain name referenced in the query does not exist");
 			break;
 		case 0x4:
-			System.out.println("ERROR\t Name server does not support the requested kind of query");
+			System.out.println("ERROR\t Name server does not support the request kind of query");
 			break;
 		case 0x5:
 			System.out.println("ERROR\t Name server refuses to perform the requested operation for policy reasons");
@@ -98,77 +98,6 @@ public class DnsClient {
 
 		printAnswerContent(responsePacket.answer, "Answer");
 		printAnswerContent(responsePacket.additional, "Additional");
-
-		System.out.println("\nHex dump request packet:");
-		printHexDump(requestPacket.message);
-
-		System.out.println("\nHex dump response packet:");
-		printHexDump(responseData);
-
-		System.out.println("timeout : " + timeout);
-		System.out.println("max retries : " + maxRetries);
-		System.out.println("port : " + port);
-		System.out.println("query type: " + queryType.name());
-		System.out.println("dns addr: " + dnsServerAddr);
-		System.out.println("dom name: " + domName);
-
-		System.out.println("id: " + responsePacket.header.randID);
-		System.out.println("QR: " + responsePacket.header.QR);
-		System.out.println("OPCODE: " + responsePacket.header.OPCODE);
-		System.out.println("AA: " + responsePacket.header.AA);
-		System.out.println("TC: " + responsePacket.header.TC);
-		System.out.println("RD: " + responsePacket.header.RD);
-		System.out.println("RA: " + responsePacket.header.RA);
-		System.out.println("Z: " + responsePacket.header.Z);
-		System.out.println("RCODE: " + responsePacket.header.RCODE);
-		System.out.println("QDCOUNT: " + responsePacket.header.QDCOUNT);
-		System.out.println("ANCOUNT: " + responsePacket.header.ANCOUNT);
-		System.out.println("NSCOUNT: " + responsePacket.header.NSCOUNT);
-		System.out.println("ARCOUNT: " + responsePacket.header.ARCOUNT);
-
-		System.out.println();
-		System.out.println();
-
-		System.out.println("Answer: ");
-
-		for (int i = 0; i < responsePacket.answer.rrs.length; i++) {
-			System.out.println("NAME: " + responsePacket.answer.rrs[i].NAME);
-			System.out.println("TYPE: " + responsePacket.answer.rrs[i].TYPE);
-			System.out.println("CLASS: " + responsePacket.answer.rrs[i].CLASS);
-			System.out.println("TTL: " + responsePacket.answer.rrs[i].TTL);
-			System.out.println("RDLENGTH: " + responsePacket.answer.rrs[i].RDLENGTH);
-			System.out.println("RDATA: " + responsePacket.answer.rrs[i].RDATA);
-			System.out.println("PREFERENCE: " + responsePacket.answer.rrs[i].PREFERENCE);
-			System.out.println("EXCHANGE: " + responsePacket.answer.rrs[i].EXCHANGE);
-			System.out.println();
-		}
-
-		System.out.println("\nAuthority: ");
-
-		for (int i = 0; i < responsePacket.auth.rrs.length; i++) {
-			System.out.println("NAME: " + responsePacket.auth.rrs[i].NAME);
-			System.out.println("TYPE: " + responsePacket.auth.rrs[i].TYPE);
-			System.out.println("CLASS: " + responsePacket.auth.rrs[i].CLASS);
-			System.out.println("TTL: " + responsePacket.auth.rrs[i].TTL);
-			System.out.println("RDLENGTH: " + responsePacket.auth.rrs[i].RDLENGTH);
-			System.out.println("RDATA: " + responsePacket.auth.rrs[i].RDATA);
-			System.out.println("PREFERENCE: " + responsePacket.auth.rrs[i].PREFERENCE);
-			System.out.println("EXCHANGE: " + responsePacket.auth.rrs[i].EXCHANGE);
-			System.out.println();
-		}
-
-		System.out.println("\nAdditional: ");
-		for (int i = 0; i < responsePacket.additional.rrs.length; i++) {
-			System.out.println("NAME: " + responsePacket.additional.rrs[i].NAME);
-			System.out.println("TYPE: " + responsePacket.additional.rrs[i].TYPE);
-			System.out.println("CLASS: " + responsePacket.additional.rrs[i].CLASS);
-			System.out.println("TTL: " + responsePacket.additional.rrs[i].TTL);
-			System.out.println("RDLENGTH: " + responsePacket.additional.rrs[i].RDLENGTH);
-			System.out.println("RDATA: " + responsePacket.additional.rrs[i].RDATA);
-			System.out.println("PREFERENCE: " + responsePacket.additional.rrs[i].PREFERENCE);
-			System.out.println("EXCHANGE: " + responsePacket.additional.rrs[i].EXCHANGE);
-			System.out.println();
-		}
 	}
 
 	public static void printAnswerContent(DnsPacketAnswer answer, String s) {
